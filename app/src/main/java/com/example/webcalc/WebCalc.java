d8bdf542123b2f33eca4ee233dd185c804968e92
Elijah <elijate@gmail.com>
2019-12-21 17:23:33 +0300

    Create a model class that extends SugarRecord

---
+++ app/src/main/java/com/example/webcalc/WebCalc.java
@@ -0,0 +1,62 @@
+package com.example.webcalc;
+
+import com.orm.SugarRecord;
+
+public class WebCalc extends SugarRecord {
+    private String numberOne;
+    private String numberTwo;
+    private String response;
+    private String expected;
+    private String passed;
+
+    public WebCalc() {
+    }
+
+    public WebCalc(String numberOne, String numberTwo, String response, String expected, String passed) {
+        this.numberOne = numberOne;
+        this.numberTwo = numberTwo;
+        this.response = response;
+        this.expected = expected;
+        this.passed = passed;
+    }
+
+    public String getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberOne(String numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public String getNumberTwo() {
+        return numberTwo;
+    }
+
+    public void setNumberTwo(String numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getExpected() {
+        return expected;
+    }
+
+    public void setExpected(String expected) {
+        this.expected = expected;
+    }
+
+    public String getPassed() {
+        return passed;
+    }
+
+    public void setPassed(String passed) {
+        this.passed = passed;
+    }
+}
